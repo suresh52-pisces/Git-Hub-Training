ea661d0034330e31c4d2096c581255431b254dbf
suresh52-pisces <suresh52.pisces@gmail.com>
2025-11-14 12:15:45 +0530

    Adding my first main.tf file to pull KeyPairs data from AWS.

---
+++ main.tf
@@ -0,0 +1,20 @@
+#Writing the program for creating an ec2 instance
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.99.1"
+    }
+  }
+}
+
+/*resource "aws_instance" "terraform-instance1" {
+  count = 2
+  ami 			= "ami-087d1c9a513324697"
+  instance_type = "t2.micro"
+}*/
+
+data "aws_key_pair" "My-KeyPair" {
+  key_name = "Aws-Cli-KP"
+}
